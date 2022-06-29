89daa2742b0bab1ceddf65f217605534079d45fd
r0otx <r00tx@mail.ru>
2022-06-30 00:43:24 +0400

    Add complete order Random order number

--- src/components/Content/Order/Order.jsx
+++ src/components/Content/Order/Order.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Breadcrumbs from "../../../utils/Breadcrumbs/Breadcrumbs";
+import classNames from "classnames";
+import "./Order.scss";
 
 const Order = ({breadcrumbLinks}) => {
+
+    const order = Math.ceil(Math.random() * 9999)
 
     return (
         <div>
             <Breadcrumbs pages={breadcrumbLinks} />
-            <div>Order</div>
+            <div className={classNames('order')}>
+            <div className={classNames('order__thanks')}>
+                Thank you for your purchase!
+            </div>
+            <div className={classNames('order__number')}>
+                Order# {order}
+            </div>
+            </div>
         </div>
     );
 };
